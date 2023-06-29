a7ee5975c3877dfef2454c395e8d3109f33d2fe4
Vladimir Mikryukov <v.mikryukov@gmail.com>
2023-06-29 12:59:30 +0300

    reformat fastx uniques - make params

--- bin/reformat_fastx_uniques_output.py
+++ bin/reformat_fastx_uniques_output.py
@@ -12,17 +12,16 @@ from Bio import SeqIO
 parser = argparse.ArgumentParser(
     description="Script to format vsearch --fastx_uniques output to restore original sequence."
 )
-parser.add_argument("run_id", help="Need run id in numeric format!")
+
+parser.add_argument("--infile",  help="Input file")
+parser.add_argument("--outfile", help="Output file")
+
 args = parser.parse_args()
 
-# read in args
-run_id = args.run_id
-if not run_id.isdigit():
-    raise ValueError("Run id is not numeric", run_id)
-
-user_dir = Path(f"{os.getcwd()}/userdir/{run_id}")
-infile = user_dir / f"source_{run_id}_fastauc"
-outfile = user_dir / f"source_{run_id}_fastanames"
+## Read in args
+# user_dir = Path(f"{os.getcwd()}/userdir/{run_id}")
+infile  = args.infile   # user_dir / f"source_{run_id}_fastauc"
+outfile = args.outfile  # user_dir / f"source_{run_id}_fastanames"
 
 matches_dict = {}
 with open(infile, "r") as i:
