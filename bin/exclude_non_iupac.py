b26ad9014931d6b6084a5fbed0a8254c25ff040a
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-05-21 13:27:16 +0300

    `exclude_non_iupac` sync with `90a99aa`

--- bin/exclude_non_iupac.py
+++ bin/exclude_non_iupac.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description="Script to remove sequences with mo
 
 parser.add_argument("--infile",      default="seqs_out_chim.fasta",  help="Input file")
 parser.add_argument("--infile_orig", default="source_fasta_unique",  help="Input originalfile")
+parser.add_argument("--cov100_uniq_file", default="source_fastanames",   help="List of duplicate sequences in user's input data")
 parser.add_argument("--outfile",     default="iupac_out_full.fasta", help="Output file")
 parser.add_argument("--outfile_vsearch_96", default="iupac_out_vsearch_96.fasta", help="Output vsearch file")
 parser.add_argument("--log_file",    default="err.log",      help="Log file")
@@ -32,6 +33,7 @@ infile             = args.infile              # user_dir / "seqs_out_chim.fasta"
 infile_orig        = args.infile_orig         # user_dir / f"source_{run_id}_fastaunique"
 outfile            = args.outfile             # user_dir / "iupac_out_full.fasta"
 outfile_vsearch_96 = args.outfile_vsearch_96  # user_dir / "iupac_out_vsearch_96.fasta"
+cov100_uniq_file   = args.cov100_uniq_file    # user_dir / f"source_{run_id}_fastanames"
 log_file           = args.log_file            # user_dir / f"err_{run_id}.log"
 ex_file            = args.ex_file             # user_dir / f"excluded_{run_id}.txt"
 allowed_number     = args.allowed_number
@@ -51,6 +53,14 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level="INFO",
 )
+
+# include info about duplicate sequences
+cov100_uniq_dict = {}
+with open(cov100_uniq_file, "r") as f:
+    dataReader = csv.reader(f, delimiter="\t")
+    for row in dataReader:
+        # include only those sequences where duplicates are present
+        cov100_uniq_dict[row[0]] = row[1]
 
 # use the original sequence for vsearch 100% clustering and not the one that came out from ITSx extractor
 orig_seqs_dict = {}
@@ -86,27 +96,19 @@ with open(ex_file, "a") as ex, open(outfile, "w") as o, open(outfile_vsearch_96,
                     numberofIUPACs2_1 = len(listOfmatches2_1)
                     numberofIUPACs2_2 = len(listOfmatches2_2)
                     if numberofIUPACs2_2 > int(allowed_number) or numberofIUPACs2_1 > 16:
-                        logging.info(f"IUPAC\tIUPAC PROBLEM: {name} ({numberofIUPACs_1}, {numberofIUPACs_2}, cut)")
-                        ex.write(
-                            f"{name}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in cut sequence exceeds the number of allowed ambiguous bases (16, {allowed_number})\n"
-                        )
+                        logging.info(f"IUPAC\tIUPAC PROBLEM: {cov100_uniq_dict[name]} ({numberofIUPACs_1}, {numberofIUPACs_2}, cut)")
+                        ex.write(f"{cov100_uniq_dict[name]}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in cut sequence exceeds the number of allowed ambiguous bases (16, {allowed_number})\n")
                     else:
                         o.write(f">{name}\n")
                         o.write(f"{part_1}\n")
                         o2.write(f">{name}\n")
                         o2.write(f"{orig_seqs_dict[name]}\n")
                 else:
-                    logging.info(
-                        f"IUPAC\tIUPAC PROBLEM: {name} ({numberofIUPACs_1}, {numberofIUPACs_2}, cut but too short to fix)"
-                    )
-                    ex.write(
-                        f"{name}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in sequence exceeds the number of allowed ambiguous bases (16, {allowed_number}). Cut, but too short to fix.\n"
-                    )
+                    logging.info(f"IUPAC\tIUPAC PROBLEM: {cov100_uniq_dict[name]} ({numberofIUPACs_1}, {numberofIUPACs_2}, cut but too short to fix)")
+                    ex.write(f"{cov100_uniq_dict[name]}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in sequence exceeds the number of allowed ambiguous bases (16, {allowed_number}). Cut, but too short to fix.\n")
             else:
-                logging.info(f"IUPAC\tIUPAC PROBLEM: {name} ({numberofIUPACs_1}, {numberofIUPACs_2})")
-                ex.write(
-                    f"{name}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in sequence exceeds the number of allowed ambiguous bases (16, {allowed_number})\n"
-                )
+                logging.info(f"IUPAC\tIUPAC PROBLEM: {cov100_uniq_dict[name]} ({numberofIUPACs_1}, {numberofIUPACs_2})")
+                ex.write(f"{cov100_uniq_dict[name]}\tIUPAC\tThe number of ambiguous bases ({numberofIUPACs_1}, {numberofIUPACs_2}) in sequence exceeds the number of allowed ambiguous bases (16, {allowed_number})\n")
         else:
             o.write(f">{name}\n")
             o.write(f"{seq}\n")
